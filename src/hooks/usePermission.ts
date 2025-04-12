ee96aac657d4881c1ecbd4663022a832c33baabe
Devin Tang <3222561952@qq.com>
2025-04-12 18:24:06 +0800

    fix: 解决商品信息不显示的问题

--- src/hooks/usePermission.ts
+++ src/hooks/usePermission.ts
@@ -3,6 +3,11 @@ import { useStore } from 'vuex'
 export function usePermission(pageName: string, handleName: string) {
   const store = useStore()
   const permissions = store.state.login.permissions
-  const verifyPermission = `system:${pageName.slice(1)}:${handleName}`
+  let verifyPermission = ''
+  if (pageName.slice(1) === 'category' || pageName.slice(1) === 'goods') {
+    verifyPermission = `product:${pageName.slice(1)}:${handleName}`
+  } else {
+    verifyPermission = `system:${pageName.slice(1)}:${handleName}`
+  }
   return Boolean(permissions.find((item: any) => item === verifyPermission))
 }
